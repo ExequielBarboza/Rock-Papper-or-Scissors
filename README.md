1dc650d19930ec057b4ca84f12ac69feca7b7160
Exequiel <exequielbarboza@gmail.com>
2024-01-14 22:49:16 -0300

    Fix the errors,until the moment the game can be play a round and return if the player win or lose.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Rock-Papper-or-Scissors
 This is a simple version of popular game "Rock, Pappel and Scissors", made to The Odin Project course.
+
+Pseudocode
+## Part 1 ♥
+
+// Begin with a function called *getComputerChoice* ♥
+// the function should choose a number between 3 options and then asign the string to the number. ♥
+// the above function should randomly return a string with *rock*, *paper* and *scissors* ♥
+// This function will be use to make the PC play, for this it should return a *return* ♥
+// Use the console to make sure this is the expected output result ♥
+
+## Part 2
+// Write a function that play a single round of Rock, Paper and Scissors.♥ 
+// The function should take 2 parameters (*playerSelection* and *computerSelection*) ♥
+// *playerSelection* function should be a option that player input and then convert the input to lower case. ♥
+// then compare its value to *computerSelection* and return a String declaring if player win or lose and why.♥
+// This function should end with a *return* ♥
+// Create a new variable to count the number of Ties.
+
+## Part 3
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,21 +1,57 @@
 
+// First step: Computer choose one option!
 
-function getComputerChoice () {
-    let randomOption = Math.floor(Math.random()*4) + 1;
-    switch (randomOption) {
+function getComputerChoice() {
+
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+
+    switch (computerChoice) {
             case 1:
-            console.log("Rock")
-        break;
+                computerChoice = "rock";
+                break;
             case 2:
-            console.log ("Pappel")
-        break;
+                computerChoice = "paper";
+                break;
             case 3:
-            console.log("Scissors")
-        break;
+                computerChoice = "scissors";
+                break;
     }
+    return computerChoice;
 }
 
-getComputerChoice();
 
-// Make the computer ramdonly select between "Rock, pappel and scissors"
-// 
+// Part 2: where the player enters his/her option and plays a round!.
+
+
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+const playerSelection = prompt("Enter a option: ");
+
+function playRound(playerSelection,computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection){
+        return("It´s a Tie!")
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        return("You lose! Paper beats rock");
+    } else if(playerSelection === "rock" && computerSelection === "scissors") {
+        return("You win! Rock beats scissors");
+    } else if(playerSelection === "paper" && computerSelection === "rock") {
+        return("You win! Paper beats rock");
+    } else if(playerSelection === "paper" && computerSelection === "scissors") {
+        return("You lose! Scissors beats paper");
+    } else if(playerSelection === "scissors" && computerSelection === "rock") {
+        return("You lose! Rock beats scissors");
+    } else if(playerSelection === "scissors" && computerSelection === "paper") {
+        return("You win! Scissors beats paper");
+    } else {
+        return("error");
+    }
+
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+// Rock = 1; Pappel = 2; Scissors = 3
