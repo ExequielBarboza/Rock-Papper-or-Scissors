2041e61487b0065aa8c151c7a597bbd267bd69db
Exequiel <exequielbarboza@gmail.com>
2024-01-26 10:18:46 -0300

    changes in game functions and add a UI to the game

--- main.js
+++ main.js
@@ -2,72 +2,74 @@
 // First step: Computer choose one option!
 
 function getComputerChoice() {
-
     let computerChoice = Math.floor(Math.random()*3) + 1;
-
     switch (computerChoice) {
             case 1:
-                computerChoice = "rock";
+                computerChoice = "Rock";
                 break;
             case 2:
-                computerChoice = "paper";
+                computerChoice = "Paper";
                 break;
             case 3:
-                computerChoice = "scissors";
+                computerChoice = "Scissors";
                 break;
     }
-    console.log("Computer choice is: " + computerChoice); // First console output
     return computerChoice;
 }
 
-
-// Part 2: where the player enters his/her option and plays a round!.
-
 let computerSelection = getComputerChoice();
-
-let playerOption = " ";
-
-function getPlayerOption(){
-    playerOption = prompt("Enter a option: ");
-    console.log("Your option is: " + playerOption);
-    return(playerOption.toLowerCase());
-}
-
-playerOption = getPlayerOption();
-
-function playRound(playerOption,computerSelection) {
-
-    if (playerOption === computerSelection){
-        return("It´s a Tie!")
-    } else if (playerOption === "rock" && computerSelection === "paper"){
-        return("You lose! Paper beats rock");
-    } else if(playerOption === "rock" && computerSelection === "scissors") {
-        return("You win! Rock beats scissors");
-    } else if(playerOption === "paper" && computerSelection === "rock") {
-        return("You win! Paper beats rock");
-    } else if(playerOption === "paper" && computerSelection === "scissors") {
-        return("You lose! Scissors beats paper");
-    } else if(playerOption === "scissors" && computerSelection === "rock") {
-        return("You lose! Rock beats scissors");
-    } else if(playerOption === "scissors" && computerSelection === "paper") {
-        return("You win! Scissors beats paper");
-    } else {
-        return("error");
-    }
-
-}
-
-console.log(playRound(playerOption,computerSelection)); // Second console output
-
-// Part 3: Completing the game.
 
 let winPlayer = 0;
 let winComputer = 0;
 let numberTies = 0;
 
-function game() {
-    
-    switch (playRound(playerOption,computerSelection)) {
+// Part 2: where the player enters his/her option and plays a round!.
+
+let result = " ";
+let playerOption = document.querySelectorAll("button");
+const yourOption = document.getElementById("results");
+
+playerOption.forEach (function(button,index) {
+    button.addEventListener("click", () => {
+        playerOption = button.textContent;
+        yourOption.innerHTML = "Results:<br>Your option is: " + playerOption +"<br>"+
+                               "Computer choice is: " + computerSelection ; 
+        const resultRound = document.createElement("div");
+        yourOption.appendChild(resultRound);
+        resultRound.textContent = playRound(playerOption,computerSelection);
+        result = resultRound.textContent;
+        game();
+        updateResults();
+        computerSelection = getComputerChoice();
+        declarateWinner();
+    });
+});
+
+function playRound(playerOption,computerSelection) {
+
+    if (playerOption === computerSelection){
+        return("It´s a Tie!")
+    } else if (playerOption === "Rock" && computerSelection === "Paper"){
+        return("You lose! Paper beats rock");
+    } else if(playerOption === "Rock" && computerSelection === "Scissors") {
+        return("You win! Rock beats scissors");
+    } else if(playerOption === "Paper" && computerSelection === "Rock") {
+        return("You win! Paper beats rock");
+    } else if(playerOption === "Paper" && computerSelection === "Scissors") {
+        return("You lose! Scissors beats paper");
+    } else if(playerOption === "Scissors" && computerSelection === "Rock") {
+        return("You lose! Rock beats scissors");
+    } else if(playerOption === "Scissors" && computerSelection === "Paper") {
+        return("You win! Scissors beats paper");
+    } else {
+        return("error");
+    }
+}
+
+// Part 3: Completing the game.
+
+function game() {    
+    switch (result) {
         case "It´s a Tie!":
             numberTies++;
             break;
@@ -81,43 +83,48 @@ function game() {
         case "You win! Scissors beats paper":
             winPlayer++;
             break;
-        default:
-            numberTies++;
     }
-    console.log("You won " + winPlayer + " times.");
-    console.log("You lose " + winComputer + " times.");
-    console.log("Number of ties: " + numberTies);
-
 }
-game(); 
+
+// Part 4: Results
+const container = document.getElementById("container");
+const results = document.createElement("div");
+const score = document.createElement("div");
+score.textContent = "Score:"
+results.appendChild(score);
+results.style.border = "solid black 10px"; // Border
+results.style.padding = "40px"; // Padding
+results.style.backgroundColor = "white"; // background color
+results.style.fontSize = "25px"; // Font size
+results.style.marginBottom = "20px";
+
+container.appendChild(results);
+const resultWins = document.createElement("div");
+results.appendChild(resultWins);
+const resultLoses = document.createElement("div");
+results.appendChild(resultLoses);
+const resultTies = document.createElement("div");
+results.appendChild(resultTies);
+
+function updateResults () {
+    resultWins.textContent = "You win "+ winPlayer +" times.";
+    resultLoses.textContent = "You lose "+ winComputer +" times.";
+    resultTies.textContent = numberTies +" ties!";
+};
 
 
-function playGame() {
-    playerOption = getPlayerOption();
-    computerSelection = getComputerChoice();
-    playRound(playerOption,computerSelection);
-    game();
-    
-    playerOption = getPlayerOption();
-    computerSelection = getComputerChoice();
-    playRound(playerOption,computerSelection);
-    game();
+function declarateWinner () {
+    if(winPlayer === 5 ){
+        winPlayer = 0;
+        winComputer = 0;
+        numberTies = 0;
+        alert("Game finished! You won! Play again!");
+    } else if (winComputer === 5){
+        winPlayer = 0;
+        winComputer = 0;
+        numberTies = 0;
+        alert("Game finished! You lose! Play again!");
+    } 
+}
 
-    playerOption = getPlayerOption();
-    computerSelection = getComputerChoice();
-    playRound(playerOption,computerSelection);
-    game();
-
-    playerOption = getPlayerOption();
-    computerSelection = getComputerChoice();
-    playRound(playerOption,computerSelection);
-    game();
-
-    playerOption = getPlayerOption();
-    computerSelection = getComputerChoice();
-    playRound(playerOption,computerSelection);
-    game();
-    }
-
-playGame();
 // Rock = 1; Pappel = 2; Scissors = 3
